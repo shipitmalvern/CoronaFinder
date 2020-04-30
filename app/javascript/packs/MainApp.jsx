042cd4ad887e8b4b73456076e495624c761ee8fa
Dalvir Singh <dalvir.singh@cerner.com>
2020-04-30 16:17:42 -0400

    Added React Routing

--- app/javascript/packs/MainApp.jsx
+++ app/javascript/packs/MainApp.jsx
@@ -6,11 +6,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import PropTypes from 'prop-types'
 import TraigeComponent from '../components/traige_component'
-
+import App from '../components/App'
 
 document.addEventListener('DOMContentLoaded', () => {
   ReactDOM.render(
-    <TraigeComponent/>,
+    <App/>,
     document.body.appendChild(document.createElement('div')),
   )
 })
